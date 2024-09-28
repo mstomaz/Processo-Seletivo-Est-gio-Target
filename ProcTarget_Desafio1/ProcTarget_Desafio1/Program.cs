630afbfce7e2763bd3cd56323b9648f9c26ba808
Mateus Tomaz <mstomaz11@gmail.com>
2024-09-28 20:24:03 -0300

    Desafios 1 e 2 do processo

---
+++ ProcTarget_Desafio1/ProcTarget_Desafio1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ProcTarget_Desafio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe um número inteiro: ");
+            int targetNumber = int.TryParse(Console.ReadLine().Trim(), out int result) ? result : 0;
+
+            if (FibonacciHasTargetNumber(targetNumber))
+                Console.WriteLine("\nO número escolhido, '{0}', pertence à sequência de Fibonacci!", targetNumber);
+            else
+                Console.WriteLine("\nO número escolhido, '{0}', NÃO pertence à sequência de Fibonacci.", targetNumber);
+
+        }
+
+        static bool FibonacciHasTargetNumber(int targetNumber)
+        {
+            int previousValue = 0, nextValue = 1;
+            for (int currentValue = 0; currentValue < targetNumber; currentValue = previousValue)
+            {
+                previousValue = nextValue;
+                nextValue += currentValue;
+            }
+            return targetNumber == previousValue;
+        }
+    }
+}
---
+++ ProcTarget_Desafio2/ProcTarget_Desafio2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.Serialization;
+
+namespace ProcTarget_Desafio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const char chosenLetter = 'a';
+
+            Console.Write("Digite uma frase qualquer: ");
+            string chosenPhrase = Console.ReadLine().Trim();
+
+            int chosenLetterCount = VerifyLetterExistenceInString(chosenPhrase, chosenLetter);
+            if (chosenLetterCount > 0)
+                Console.WriteLine($"A letra '{chosenLetter}' está presente na frase {chosenLetterCount} vezes.");
+            else
+                Console.WriteLine($"A letra '{chosenLetter}' não está presente na frase.");
+        }
+
+        static int VerifyLetterExistenceInString(string phrase, char chosenLetter)
+        {
+            char upperChosenLetter = char.Parse(chosenLetter.ToString().ToUpper());
+            int letterAmount = 0;
+            foreach (char letter in phrase.ToUpper())
+            {
+                if (letter == upperChosenLetter)
+                    letterAmount++;
+            }
+            return letterAmount;
+        }
+    }
+}
